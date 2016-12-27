91f087c40c25f005467870e9298e389229e1c913
Sam Messina <samuel.messina@gmail.com>
2016-12-27 14:37:13 -0600

    Search now works and sends query to results via URL parameter

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import PropTypes from 'react';
 import MainContainer from './MainContainer.js';
+import {space} from '../styles';
 
 class Search extends React.Component {
+    constructor() {
+        super();
+        this.state = {artist:''}
+        this.onSubmitArtist = this.onSubmitArtist.bind(this);
+        this.onUpdateArtist = this.onUpdateArtist.bind(this);
+    }
+    onSubmitArtist(e) {
+        e.preventDefault();
+        console.table(this.props);
+        this.context.router.push({
+            pathname: '/results',
+            query: {
+                artist: this.state.artist
+                }
+            });
+
+    }
+    onUpdateArtist(e) {
+        this.setState({
+            artist: e.target.value
+        });
+    }
     render() {
         return (
             <MainContainer>
                 <h1>Find an artist</h1>
-                <form onSubmit={props.onSubmitArtist}>
-                    <label for="artist">Artist</label>
-                    <input id="artist" name="artist" type="text" value="artist"/>
-                    <button className="btn btn=lg btn-black" type="submit">Submit</button>
-                    
-                </form>
+                <div className="col-sm-6 col-sm-offset-3">
+                    <form onSubmit={this.onSubmitArtist}>
+                        <div className="form-group">
+                            <input id="artist"
+                                   name="artist"
+                                   type="text"
+                                   className="form-control"
+                                   placeholder="Artist"
+                                   onChange={this.onUpdateArtist}
+                                   value={this.state.artist} />
+                            <br/>
+                            <button className="btn btn=lg btn-black" style={space} type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </MainContainer>
 
         )
     }
 }
-Search.propTypes = {
-  onSubmitArtist : React.PropTypes.func.isRequired,
-}
+Search.contextTypes = {
+    router: React.PropTypes.object.isRequired
+};
+
 
 export default Search;
 
