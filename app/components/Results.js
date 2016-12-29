ab9f5c6219c04b7ae51b017eab27a3d506dd9273
Sam Messina <samuel.messina@gmail.com>
2016-12-29 02:07:45 -0600

    Got results working!!

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,40 +4,50 @@ import MainContainer from './MainContainer';
 import ArtistInfo from './ArtistInfo';
 import {space} from '../styles';
 import queryHelpers from '../utils/queryHelpers';
+import Loading from './Loading';
+import ResultsContainer from '../containers/ResultsContainer';
 
-
-class Results extends React.Component {
-    constructor(props) {
+class Results extends React.Component{
+    constructor(props){
         super(props);
-        this.state= {
-            artist: '',
+        this.artistList = [];
+        this.state = {
+            isLoading:  false,
+            query: this.props.query,
             artistInfo: []
         }
     }
-    componentWillMount() {
-        let query = this.props.location.query;
-        queryHelpers.getArtistInfo(query.artist)
-                    .then(function(result) {
-                        console.log(result);
-                        this.setState({
-                            artist: query.artist,
-                            artistInfo: result
-                        });
-                    }.bind(this))
-                    .catch(function (error) {
-                        console.log(error);
-                    });
+    componentDidMount() {
+        if(this.props.artistInfo.length > 0) {
+            this.setState({
+                isLoading: this.props.isLoading,
+                artistInfo: this.props.artistInfo
+            });
+            let i = 0;
+            this.artistList = this.props.artistInfo.map((artist) => 
+                <ArtistInfo
+                    key={artist.id}
+                    _name={artist.name}
+                    _image={artist.image}
+                    _href={artist.href}
+                    _genre={artist.genre}
+                />
+            );
+        }
     }
-
     render() {
         return (
+            this.state.isLoading ?
+            <Loading text="Waiting"/>:
             <MainContainer>
-                <h1>Results for: {this.state.artist}</h1>
+                <h1>Results for: {this.state.query}</h1>
+                {this.artistList}
             </MainContainer>
-
         )
     }
 }
+Results.propTypes = {
+}
+
 
 export default Results;
-
